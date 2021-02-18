201b77c48e608912336c69bb05a08e0cd88a7b37
Vashisth <yash.vashisth@accenture.com>
2021-02-18 09:34:01 +0530

    week 4-PArt1-Add-Comments

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import About from './AboutComponent';
 import { Switch, Route, Redirect ,withRouter } from 'react-router-dom';
 import Home from './HomeComponent';
 import { connect } from 'react-redux';
+import { addComment } from '../redux/ActionCreators';
 // import { COMMENTS } from '../shared/comments';
 // import { PROMOTIONS } from '../shared/promotions';
 // import { LEADERS } from '../shared/leaders';
@@ -23,6 +24,11 @@ const mapStateToProps = state => {
   }
 }
 
+const mapDispatchToProps = dispatch => ({
+  
+  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+
+});
 class Main extends Component {
 
   constructor(props) {
@@ -46,8 +52,10 @@ class Main extends Component {
 
     const DishWithId = ({match}) => {
       return(
-          <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-            comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+        <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+        comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+        addComment={this.props.addComment}
+      />
       );
     };
     return (
@@ -74,4 +82,4 @@ class Main extends Component {
   }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,29 @@
-import {createStore} from 'redux';
-import { Reducer, initialState } from './reducer'
+import {createStore, combineReducers} from 'redux';
+import { Dishes } from './dishesReducer';
+import { Comments } from './commentsReducer';
+import { Promotions } from './promotionsReducer';
+import { Leaders } from './leadersReducer';
+
 
 export const ConfigureStore = () => {
-    const store = createStore(
-        Reducer, // reducer
-        initialState, // our initialState
-    );
+        const store = createStore(
+            combineReducers({
+                dishes: Dishes,
+                comments: Comments,
+                promotions: Promotions,
+                leaders: Leaders
+            })
+        );
+    
+        return store;
+    }
 
-    return store;
-}
+
+// export const ConfigureStore = () => {
+//     const store = createStore(
+//         Reducer, // reducer
+//         initialState, // our initialState
+//     );
+
+//     return store;
+// }
